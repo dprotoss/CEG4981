ef75cf3701288924b7b0ad553517e7816e5db7d4
root <root@LAPTOP-R3LKUEJG>
2023-11-28 10:15:30 -0500

    skeleton for sending requests out (need to fix connect and make static IP)

--- src/app.js
+++ src/app.js
@@ -6,11 +6,62 @@ const stuff = Vue.createApp({
         }
     },
 methods: {
-    toggleFlash(){
-        this.isFlashing = !this.isFlashing
+    async toggleFlash() {
+        this.isFlashing = !this.isFlashing;
+        const res = await helloRequest();
+        const square = document.getElementById('squareThang');
+        
+        console.log("squareID:", square);
+        console.log("toggle flash debug:", res);
+
+        if (res === "Hello World!") {
+            square.style.backgroundColor = "green";
+            console.log("I made it inside the if statement!");
+        }
     }
 }
 })
+
+
+async function sendRequest(endpoint){
+    try {
+        const response = await fetch(`http://192.168.2.3${endpoint}`);
+        const data = await response.text();
+        return data; // This will be a Promise that resolves to 'data'
+    } catch (error) {
+        console.error('Fetch Error:', error);
+    }
+}
+
+async function helloRequest() {
+    try {
+        const data = await sendRequest("/hello");
+        console.log(data);
+        return data;
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+async function stopRequest() {
+    try {
+        const data = await sendRequest("/stop");
+        return data;
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+async function startRequest() {
+    console.log("I EXIST");
+    try {
+        const data = await sendRequest("/start");
+        return data;
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
 
 
 stuff.mount('#app')
